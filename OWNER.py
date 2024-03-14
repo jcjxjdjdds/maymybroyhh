07352f4fc4d75960de7c779095f990bb2c439a44
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2024-03-14 14:17:12 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -2,7 +2,7 @@
 OWNER = ["T4_Mohamed", "T4_Mohamed"]
 OWNER_NAME = "👿Mohamedّ"
 BOT_TOKEN = "7133439731:AAG9VK6hm-Hn962-Hn4DKuwVojdIm0sEIsE"
-DATABASE = "mongodb+srv://mohamedygtyg:gLvHNqoUITATbUMT@cluster0.pouornu.mongodb.net/"
+DATABASE = "mongodb+srv://khaledmosad:AfLmwiqPzqH9GIJx@cluster0.6zudvlp.mongodb.net/"
 CHANNEL = "https://t.me/k_40_x"
 GROUP = "https://t.me/k_40_x"
 VIDEO = "https://t.me/HLV_M/720"
